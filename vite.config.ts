82aee6d99504f4af648818bceba181366e4ad99e
Vlad Pescari <vlad.pescari@softwaremind.com>
2025-02-17 14:25:57 +0200

    added deploy instructions for github

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dnd',
+  base: 'dnd-git-main-vlad2226s-projects',
   server: {
     port: 3090
   },
